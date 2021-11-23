9ff5756030d5f245a0fa2ff07132bc0c2d3fe46f
MateoMaldonado3 <maldonadom377@gmail.com>
2021-11-22 23:13:29 -0500

    Classesv1.0

---
+++ 06-Code/Application of human resources/src/ec/edu/espe/farm/model/Company.java
@@ -0,0 +1,16 @@
+
+package ec.edu.espe.farm.model;
+
+public class Company {
+        
+    private int hoursOfAttention;
+    private int moneyEarned;
+    private String location;
+    
+    public void MoneyEarnedInTheLastMonth(){
+             
+    }
+    public void ExpensesInTheLastMonth(){
+        
+    }
+}
---
+++ 06-Code/Application of human resources/src/ec/edu/espe/farm/model/Payment.java
@@ -0,0 +1,23 @@
+
+package ec.edu.espe.farm.model;
+
+public class Payment {
+
+    private boolean paydate;
+    private int hoursworked;
+    private String workerposition;
+    private int moneytopay;
+    
+    public boolean CheckIfisDateOfPayment(){
+               
+    return false;
+    }
+    
+    public void totalpayment(){
+        
+    }
+    
+    public void WhoAreGoingToBePaid(){
+        
+    }
+}
