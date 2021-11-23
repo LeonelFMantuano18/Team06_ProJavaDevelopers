1d58664c25a826d64aa0b52614aa8675c5873e5e
ramaisincho1 <richarwolf7@gmail.com>
2021-11-22 23:28:21 -0500

    Time

---
+++ 06-Code/Application of human resources/src/ec/edu/espe/farm/model/Time.java
@@ -0,0 +1,24 @@
+
+package objetosdelproyecto;
+
+/**
+ *
+ * @author Maisincho Richar 
+ */
+public class Time {
+    //atributos
+    String entrytime;
+    String lunchtime;
+    String departuretime;
+    
+    int time;
+    
+    //metodo
+    
+    public static void main(String[] args){
+        
+    }
+
+       
+    
+}
