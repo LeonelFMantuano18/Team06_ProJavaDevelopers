1f5d2e44963829a05a3da74b4ff3938a84895a01
Diego Sebastian Palacios Cando <sp150646@gmail.com>
2021-11-22 23:29:41 -0500

    objects

---
+++ 06-Code/Application of human resources/src/ec/edu/espe/farm/model/Payrole.java
@@ -0,0 +1,12 @@
+package payrole;
+
+public class Payrole {
+
+    private int salary;
+    private int debts;
+    
+public void payrole() {
+       
+    }
+    
+}
---
+++ 06-Code/Application of human resources/src/ec/edu/espe/farm/model/Profile.java
@@ -0,0 +1,13 @@
+package profile;
+public class Profile {
+
+    private String name;
+    private int id;
+    private String areawork;
+    private int password;
+    
+public void profile () {
+        
+    }
+    
+}
