f80ee96e3c326e09bc79d18bd733c7486c7ba131
ramaisincho1 <richarwolf7@gmail.com>
2021-11-22 23:48:39 -0500

    invoice record

---
+++ 06-Code/Application of human resources/src/ec/edu/espe/farm/model/Invoice record.java
@@ -0,0 +1,24 @@
+
+package objetosdelproyecto;
+
+/**
+ *
+ * @author Maisincho Richar 
+ */
+public class Invoice record {
+    //atributos
+     String customer; 
+    String datecustomer;
+
+    
+    
+    //metodo
+    
+    public  void main(String[] args){
+        
+    }
+    
+     public static void main(String[] args){
+        
+    }
+}
