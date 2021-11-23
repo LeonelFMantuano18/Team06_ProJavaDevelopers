bca85bf53294c81accde93ca32b51c56d0291663
ajmorales4 <javier.moralesc114477@gmail.com>
2021-11-22 23:24:01 -0500

    C:\POO\Codigos\Team06_ProJavaDevelopers

---
+++ 06-Code/Application of human resources/src/ec/edu/espe/farm/model/Employee.java
@@ -0,0 +1,21 @@
+
+package ec.edu.espe;
+
+public class Employee {
+    private String how_long_you_have_working;
+    private String how_long_before_retirement;
+    
+    public static void main(String[] args) {
+        Employee employee1 = new Employee();
+        
+    
+    }
+    public void Checkthetimeinthecompany (){
+        
+    }
+    public void Checkremainingtime (){
+        
+    }
+    
+    }
+    
---
+++ 06-Code/Application of human resources/src/ec/edu/espe/farm/model/Transportation.java
@@ -0,0 +1,19 @@
+package ec.edu.espe;
+
+public class Transportation {
+    private Boolean the_job_includes_transportation;
+    private int how_many_workers_have_a_car;
+    
+    public static void main(String[] args) {
+        Transportation transportation1 = new Transportation();
+    
+    
+    }
+    public boolean CheckforTransport(){
+    
+    return true;
+    }
+    public void CheckhowmanyhaveTransportation(){
+        
+    }
+}
