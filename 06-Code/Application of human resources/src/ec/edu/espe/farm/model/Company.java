0f1977a89f0f69afc31f09585b49b6784102e7c4
MateoMaldonado3 <maldonadom377@gmail.com>
2021-12-01 01:05:12 -0500

    Proyect getters and setters v1

--- 06-Code/Application of human resources/src/ec/edu/espe/farm/model/Company.java
+++ 06-Code/Application of human resources/src/ec/edu/espe/farm/model/Company.java
@@ -13,4 +13,51 @@ public class Company {
     public void ExpensesInTheLastMonth(){
         
     }
+
+    @Override
+    public String toString(){
+        return "Company{" + "hoursOfAttention=" + hoursOfAttention + ", moneyEarned=" + moneyEarned + ", location=" + location + '}';
+    }
+    
+    /**
+     * @return the hoursOfAttention
+     */
+    public int getHoursOfAttention() {
+        return hoursOfAttention;
+    }
+
+    /**
+     * @param hoursOfAttention the hoursOfAttention to set
+     */
+    public void setHoursOfAttention(int hoursOfAttention) {
+        this.hoursOfAttention = hoursOfAttention;
+    }
+
+    /**
+     * @return the moneyEarned
+     */
+    public int getMoneyEarned() {
+        return moneyEarned;
+    }
+
+    /**
+     * @param moneyEarned the moneyEarned to set
+     */
+    public void setMoneyEarned(int moneyEarned) {
+        this.moneyEarned = moneyEarned;
+    }
+
+    /**
+     * @return the location
+     */
+    public String getLocation() {
+        return location;
+    }
+
+    /**
+     * @param location the location to set
+     */
+    public void setLocation(String location) {
+        this.location = location;
+    }
 }
--- 06-Code/Application of human resources/src/ec/edu/espe/farm/model/Payment.java
+++ 06-Code/Application of human resources/src/ec/edu/espe/farm/model/Payment.java
@@ -7,7 +7,7 @@ public class Payment {
     private int hoursworked;
     private String workerposition;
     private int moneytopay;
-    
+      
     public boolean CheckIfisDateOfPayment(){
                
     return false;
@@ -19,5 +19,66 @@ public class Payment {
     
     public void WhoAreGoingToBePaid(){
         
+    }
+    
+    @Override
+    public String toString(){
+        return "Payment{" + "paydate=" + paydate + ", hoursworked=" + hoursworked + ", workerposition=" + workerposition + ", moneytopay=" + moneytopay + '}';
+    }
+
+/**
+     * @return the paydate
+     */
+    public boolean isPaydate() {
+        return paydate;
+    }
+
+    /**
+     * @param paydate the paydate to set
+     */
+    public void setPaydate(boolean paydate) {
+        this.paydate = paydate;
+    }
+
+    /**
+     * @return the hoursworked
+     */
+    public int getHoursworked() {
+        return hoursworked;
+    }
+
+    /**
+     * @param hoursworked the hoursworked to set
+     */
+    public void setHoursworked(int hoursworked) {
+        this.hoursworked = hoursworked;
+    }
+
+    /**
+     * @return the workerposition
+     */
+    public String getWorkerposition() {
+        return workerposition;
+    }
+
+    /**
+     * @param workerposition the workerposition to set
+     */
+    public void setWorkerposition(String workerposition) {
+        this.workerposition = workerposition;
+    }
+
+    /**
+     * @return the moneytopay
+     */
+    public int getMoneytopay() {
+        return moneytopay;
+    }
+
+    /**
+     * @param moneytopay the moneytopay to set
+     */
+    public void setMoneytopay(int moneytopay) {
+        this.moneytopay = moneytopay;
     }
 }
