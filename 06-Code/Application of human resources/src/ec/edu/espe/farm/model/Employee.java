9b9156ff9cf22b1b6d79a8567d933ea4454dde2a
ajmorales4 <javier.moralesc114477@gmail.com>
2021-12-01 01:07:45 -0500

    Proyect getters and setters

--- 06-Code/Application of human resources/src/ec/edu/espe/farm/model/Employee.java
+++ 06-Code/Application of human resources/src/ec/edu/espe/farm/model/Employee.java
@@ -16,6 +16,44 @@ public class Employee {
     public void Checkremainingtime (){
         
     }
-    
+
+    @Override
+    public String toString() {
+        return "Employee{" + "how_long_you_have_working=" + how_long_you_have_working + ", how_long_before_retirement=" + how_long_before_retirement + '}';
+    }
+
+    public Employee(String how_long_you_have_working, String how_long_before_retirement) {
+        this.how_long_you_have_working = how_long_you_have_working;
+        this.how_long_before_retirement = how_long_before_retirement;
+    }
+
+    /**
+     * @return the how_long_you_have_working
+     */
+    public String getHow_long_you_have_working() {
+        return how_long_you_have_working;
+    }
+
+    /**
+     * @param how_long_you_have_working the how_long_you_have_working to set
+     */
+    public void setHow_long_you_have_working(String how_long_you_have_working) {
+        this.how_long_you_have_working = how_long_you_have_working;
+    }
+
+    /**
+     * @return the how_long_before_retirement
+     */
+    public String getHow_long_before_retirement() {
+        return how_long_before_retirement;
+    }
+
+    /**
+     * @param how_long_before_retirement the how_long_before_retirement to set
+     */
+    public void setHow_long_before_retirement(String how_long_before_retirement) {
+        this.how_long_before_retirement = how_long_before_retirement;
+    }
+
     }
     
--- 06-Code/Application of human resources/src/ec/edu/espe/farm/model/Transportation.java
+++ 06-Code/Application of human resources/src/ec/edu/espe/farm/model/Transportation.java
@@ -16,4 +16,42 @@ public class Transportation {
     public void CheckhowmanyhaveTransportation(){
         
     }
+
+    @Override
+    public String toString() {
+        return "Transportation{" + "the_job_includes_transportation=" + the_job_includes_transportation + ", how_many_workers_have_a_car=" + how_many_workers_have_a_car + '}';
+    }
+
+    public Transportation(Boolean the_job_includes_transportation, int how_many_workers_have_a_car) {
+        this.the_job_includes_transportation = the_job_includes_transportation;
+        this.how_many_workers_have_a_car = how_many_workers_have_a_car;
+    }
+    
+    /**
+     * @return the the_job_includes_transportation
+     */
+    public Boolean getThe_job_includes_transportation() {
+        return the_job_includes_transportation;
+    }
+
+    /**
+     * @param the_job_includes_transportation the the_job_includes_transportation to set
+     */
+    public void setThe_job_includes_transportation(Boolean the_job_includes_transportation) {
+        this.the_job_includes_transportation = the_job_includes_transportation;
+    }
+
+    /**
+     * @return the how_many_workers_have_a_car
+     */
+    public int getHow_many_workers_have_a_car() {
+        return how_many_workers_have_a_car;
+    }
+
+    /**
+     * @param how_many_workers_have_a_car the how_many_workers_have_a_car to set
+     */
+    public void setHow_many_workers_have_a_car(int how_many_workers_have_a_car) {
+        this.how_many_workers_have_a_car = how_many_workers_have_a_car;
+    }
 }
