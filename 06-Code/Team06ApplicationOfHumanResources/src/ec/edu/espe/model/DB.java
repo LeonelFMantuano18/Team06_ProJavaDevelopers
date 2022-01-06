113952d7673c4f673979aa7e2eb5dd729b1416a1
Diego Sebastian Palacios Cando <sp150646@gmail.com>
2022-01-06 08:48:19 -0500

    Libraries

--- 06-Code/Team06ApplicationOfHumanResources/src/ec/edu/espe/model/DB.java
+++ 06-Code/Team06ApplicationOfHumanResources/src/ec/edu/espe/model/DB.java
@@ -13,7 +13,7 @@ import com.mongodb.client.MongoDatabase;
 public class DB {
     
 
-ConnectionString connectionString = new ConnectionString("mongodb+srv://TheTue:Thetrue?18@cluster0.tanum.mongodb.net/myFirstDatabase?retryWrites=true&w=majority");
+ConnectionString connectionString = new ConnectionString("mongodb+srv://dspalacios:dspalacios@cluster0.8plsi.mongodb.net/myFirstDatabase?retryWrites=true&w=majority");
 MongoClientSettings settings = MongoClientSettings.builder()
         .applyConnectionString(connectionString)
         .build();
