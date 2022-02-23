204a70485279fbb1baff0f79797029de195ee69d
LeonelFMantuano18 <leonelfmantuano@gmail.com>
2022-02-23 17:21:49 -0500

    Team06ApplicationOfHumanResources

--- 06-Code/Team06ApplicationOfHumanResources/src/ec/edu/espe/utils/DB.java
+++ 06-Code/Team06ApplicationOfHumanResources/src/ec/edu/espe/utils/DB.java
@@ -13,22 +13,14 @@ import com.mongodb.client.MongoDatabase;
 public class DB {
     
 
-<<<<<<< HEAD:06-Code/Team06ApplicationOfHumanResources/src/ec/edu/espe/utils/DB.java
+
 ConnectionString connectionString = new ConnectionString("mongodb+srv://TheTue:Thetrue_18@cluster0.tanum.mongodb.net/myFirstDatabase?retryWrites=true&w=majority");
-=======
-<<<<<<< HEAD
-ConnectionString connectionString = new ConnectionString("mongodb+srv://ramaisincho1:ramaisincho1@clusteroop.hjw4g.mongodb.net/myFirstDatabase?retryWrites=true&w=majority");
-=======
-ConnectionString connectionString = new ConnectionString("mongodb+srv://cmteca:1750288969@cluster0.bl97r.mongodb.net/myFirstDatabase?retryWrites=true&w=majority");
->>>>>>> f431856a6096b509bedfc8e5f8473aff44f05790
->>>>>>> c425969d13352018992ffdc32e131a3c5cc79822:06-Code/Team06ApplicationOfHumanResources/src/ec/edu/espe/model/DB.java
 MongoClientSettings settings = MongoClientSettings.builder()
         .applyConnectionString(connectionString)
         .build();
 MongoClient mongoClient = MongoClients.create(settings);
 MongoDatabase database = mongoClient.getDatabase("Project");
 
-    
     public MongoDatabase obtenerDB(){
         return database;
     }
